1223c0f659647418c582875643aff54ab65320b5
Santhosh Kumar Vaddepelli <sankumar2910@gmail.com>
2024-01-28 22:46:36 +0530

    initial nav links

---
+++ src/components/header/Navbar.js
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button, Grid, Stack } from '@mui/material';
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar color="inherit" position="fixed">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Grid container spacing={1}>
+            <Grid item md={9}>
+              <Stack spacing={1} direction="row">
+                <Button color='primary' href="/" variant="outlined">
+                  Home
+                </Button>
+              </Stack>
+            </Grid>
+            
+            <Grid item md={3}>
+              <Stack spacing={1} direction="row">
+                <Button color='primary' href="/login" variant="contained">
+                  Login
+                </Button>
+                <Button color='primary' href="/register" variant="outlined">
+                  Register
+                </Button>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Navbar from '../header/Navbar';
 import { useDispatch, useSelector } from 'react-redux';
 const Home = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   console.log(user);
   return (
-    <div>Home</div>
+    <Navbar />
   )
 }
 
